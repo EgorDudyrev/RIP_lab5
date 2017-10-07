23fba092df4a287bc9a0b7500abb2ee4480f95b1
EgorDudyrev <egor.dudyrev@yandex.ru>
2017-10-07 20:07:59 +0300

    Deleted wrong Django project

--- lab5/my_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^news/(?P<id>(\d+))$',views.single_news, name='news'),
-    url(r'^$',views.index,name='index')
-]
